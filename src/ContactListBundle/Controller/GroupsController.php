ddc389bfb415e16b1a2d49eb1ed5b2b748613413
Bartosz Nowak <bartosz.nowak@windowslive.com>
2017-04-27 14:48:10 +0200

    created groups templates and actions(add/showContactsInGroup/delete)

--- src/ContactListBundle/Controller/GroupsController.php
+++ src/ContactListBundle/Controller/GroupsController.php
@@ -69,4 +69,39 @@ class GroupsController extends Controller
 
         return ['groups' => $groups];
     }
+
+    /**
+     * @Route("/show/{id}")
+     * @Template(":groups:show_group.html.twig")
+     */
+    public function showByIdAction($id)
+    {
+        $group = $this->getDoctrine()->getRepository('ContactListBundle:Groups')->find($id);
+
+        if(!$group){
+            throw new $this->createNotFoundException('Group not found');
+        }
+
+        return ['group' => $group];
+    }
+
+    /**
+     * @Route("/delete/{id}")
+     */
+    public function deleteAction($id)
+    {
+        $group = $this->getDoctrine()->getRepository('ContactListBundle:Groups')->find($id);
+
+        if (!$group){
+            throw new $this->createNotFoundException('Group not found');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($group);
+
+        $em->flush();
+
+        return $this->redirectToRoute('contactlist_groups_showall');
+    }
 }
